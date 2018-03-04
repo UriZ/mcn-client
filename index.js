b6f2f9fb087c99239a1213dc19fe674e91f41b20
URIZ <uriz@il.ibm.com>
2018-03-04 16:32:36 +0200

    set up client and server in same app

---
+++ index.js
@@ -0,0 +1,50 @@
+let express = require('express');
+let passport = require('passport');
+let app = express();
+let path = require('path');
+
+app.set('port', (process.env.PORT || 3001));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+    // response.render('pages/index')
+
+    // response.sendFile(path.join(__dirname + '/views/pages/ReactTest.html'));
+    response.send("encouragement!!!");
+
+});
+
+
+let FacebookTokenStrategy = require('passport-facebook-token');
+
+passport.use(new FacebookTokenStrategy({
+        clientID: "1151370004993163",
+        clientSecret: "50bb09be87258f04b79883ddb4655512"
+    }, function(accessToken, refreshToken, profile, done) {
+        return done(error, profile.id);
+
+    }
+));
+
+
+app.post('/auth/facebook/token',
+    passport.authenticate('facebook-token'),
+    function (req, res) {
+        // do something with req.user
+        res.send("ok!!");
+    }
+);
+
+
+app.post('/test', function(request, response){
+    response.send("ok");
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
