9852d9a482ff1175b7ecf98e3e402782e97fc657
URIZ <uriz@il.ibm.com>
2018-04-04 23:54:06 +0300

    support for create user (signup) in client baceknd

--- index.js
+++ index.js
@@ -71,7 +71,44 @@ let getMatch = (request, response)=>{
 app.get('/match', getMatch);
 
 
+let createUser = (request, response)=>{
 
+    // get token from header
+    let tokenHeader = request.headers['authorization'];
+
+    // options for service api call
+    let options = {
+        method: 'post',
+        uri: SERVICE_API_URL+'users',
+        headers: {
+            'authorization': tokenHeader
+        },
+        json: true // Automatically parses the JSON string in the response
+    };
+
+
+    requestPromise(options)
+        .then(function (result) {
+            console.log("success creating a user");
+            response.status(200).send(result);
+        })
+        .catch(function (err) {
+
+            if (err.statusCode){
+                console.log("error crating a user : " + JSON.stringify(err.error));
+                // we use err.error since request promise transforms the err object
+                response.status(err.statusCode).send(err.error);
+            }
+            else{
+                console.log("unknown error - returning 500: " + JSON.stringify(err.error));
+                response.status(500).send(err.error);
+
+            }
+        });
+};
+
+
+app.post('/users', createUser);
 
 
 app.listen(app.get('port'), function() {
