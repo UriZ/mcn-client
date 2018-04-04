955f23b52da8a241b88371f2f01504ee0f812c35
URIZ <uriz@il.ibm.com>
2018-04-05 01:01:57 +0300

    add support for the signup flow - create a user if he does not exist

--- client/src/Login.js
+++ client/src/Login.js
@@ -18,6 +18,61 @@ class LoginPage extends React.Component {
     };
 
 
+    signUp(){
+
+        // keep that since all the FB actions are in the global scope
+        let that = this;
+
+        // user not connected to the app - need to login
+        window.FB.login((response)=>{
+
+            if (response.status === 'connected'){
+                alert(response.authResponse.accessToken);
+
+                // try and create a user in the db. If it already exists - simply continue with login. else create it and login
+
+
+                // create authorisation header
+                let accessToken = window.FB.getAuthResponse().accessToken;
+                let authHeader = "Bearer " + accessToken;
+
+                // set the loading state in the store
+                // this.props.dispatchGettingFeedData();
+
+                fetch('/users',{
+                    method: 'POST',
+                    headers: new Headers({
+                        "Content-Type": "application/json",
+                        "authorization": authHeader
+                    })
+                }).then((response) =>{
+                    if (!response.ok){
+                        response.json().then((res)=>{
+                            if (res.errorCode && res.errorCode == "USER_EXISTS"){
+                                // user is already in the db - simply login
+                                that.props.dispatchLogin();
+                            }
+                            else{
+                                // something else happened
+                                alert("some other error");
+                            }
+                        });
+                     }
+                     else{
+                        // user created ok - continue with login
+                        that.props.dispatchLogin();
+                    }
+                }).catch((error)=>{
+                    // this is some network error - show error
+                    alert("network error");
+                });
+            }
+            else{
+                // user did not login to fb - nothing happened
+            }
+        },{scope:'user_friends', return_scopes: true});
+    }
+
 
     login(){
         // keep that since all the FB actions are in the global scope
@@ -62,15 +117,30 @@ class LoginPage extends React.Component {
 
             return (
 
+                <div>
 
-                <div className="login">
-                    <h1>Login using fb</h1>
+                    <div className="login">
+                        <h1>Login using fb</h1>
 
-                    <div>
-                        {/* bind this to have access to props later on */}
-                        <button onClick={this.login.bind(this)}>Log in</button>
+                        <div>
+                            {/* bind this to have access to props later on */}
+                            <button onClick={this.login.bind(this)}>Log in</button>
+                        </div>
                     </div>
+
+
+                    <div className= "signup">
+                        <button onClick={this.signUp.bind(this)}>sign up</button>
+
+
+                    </div>
+
+
                 </div>
+
+
+
+
             );
         }
     }
