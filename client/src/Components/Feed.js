790cd14be3f19cae55e7c25e6a1accd4a8c13b61
URIZ <uriz@il.ibm.com>
2018-04-13 23:12:21 +0300

    cleanup

--- client/src/Components/Feed.js
+++ client/src/Components/Feed.js
@@ -9,43 +9,6 @@ import MutualFriends from './MutualFriends'
 import FeedCard from "./FeedCard";
 
 
-const CardExampleWithAvatar = (props ) => (
-    <Card>
-        <CardHeader
-            title={props.title}
-            subtitle={props.subtitle}
-            avatar={props.image}
-        />
-        {/*<CardMedia*/}
-        {/*overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}*/}
-        {/*>*/}
-        {/*<img src="./cover.png" alt="" />*/}
-        {/*</CardMedia>*/}
-        {/*<CardTitle title="Card title" subtitle="Card subtitle" />*/}
-        <CardText>
-            I would like to {props.preferences.operation} {props.preferences.amount} {props.preferences.currency}
-            {/*Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.*/}
-            {/*Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.*/}
-        </CardText>
-        <CardActions>
-
-            <IconButton  aria-label="Delete" onClick={()=>{alert("ok2")}}>
-                <DeleteIcon />
-            </IconButton>
-            {/*<div>*/}
-                {/*{props.mutualFriends?*/}
-                    {/*( <Avatar style= {{height:'25px', width:'25px'}}alt="Remy Sharp" src={props.mutualFriends[0].picture.data.url}/>)*/}
-                    {/*: ""}*/}
-
-            {/*</div>*/}
-            <MutualFriends mutualFriends={props.mutualFriends}/>
-            {/*<FlatButton label="Action1" onClick={()=>{alert("ok")}} />*/}
-            {/*<FlatButton label="Action2"  onClick={()=>{alert ('click 2')}}/>*/}
-        </CardActions>
-    </Card>
-);
-
-
 /**
  * given an array of feed elements - render a feed
  */
@@ -90,8 +53,6 @@ class Feed extends React.Component{
                     friendDegree = "2nd";
                 }
 
-
-
                 return (
 
                     <div>
@@ -106,10 +67,7 @@ class Feed extends React.Component{
             });
             return feed;
         }
-
-
     }
-
 }
 
 
