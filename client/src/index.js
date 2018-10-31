3bf10c5b37d48f6822597b17a96189cfed89b099
uriz <uriz@il.ibm.com>
2018-10-31 23:16:02 +0200

    cleanup comments etc, delete authentication js fie

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-// import AuthExample from './Authentication.js';
-// import LoginPage from "./Login";
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 
